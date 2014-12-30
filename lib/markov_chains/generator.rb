41eb2a4daff962502b4a01cf2acc3011323db413
justindomingue <justin.domingue@hotmail.com>
2014-12-30 12:52:05 -0500

    completed generator class, with tests

--- lib/markov_chains/generator.rb
+++ lib/markov_chains/generator.rb
@@ -1,4 +1,40 @@
 module MarkovChains
   class Generator
+    
+    # Initializes the generator
+    #
+    # @example Create a new generator
+    #   MarkovChains::Generator.new(text)
+    #
+    # @param Text source to generate sentences from
+    #
+    def initialize(text)
+      @dict = MarkovChains::Dictionary.new(text)
+    end
+    
+    # Returns a given number of randonly generated sentences
+    #
+    # @example Get 5 sentences
+    #   get_sentences(5)
+    #
+    # @param n [int] number of sentences to generate
+    # @return Array conataining generated sentences
+    #
+    def get_sentences(n)
+      sentences = []
+      
+      n.times do
+        sentence = ""
+        word = @dict.get_start_word
+        until sentence.scan(/\.|\?|!/).size == 1
+          sentence << word << " "
+          word = @dict.get(word)
+        end
+        
+        sentences.push sentence
+      end
+      
+      sentences
+    end
   end
 end
--- test/test_generator.rb
+++ test/test_generator.rb
@@ -2,4 +2,12 @@ require 'minitest/autorun'
 require 'markov_chains'
 
 class GeneratorTest < Minitest::Test
+  def setup
+    source = "Why did the Mobius Strip cross the road? To get to the same side!"
+    @generator = MarkovChains::Generator.new source
+  end
+  
+  def test_get_sentences
+    assert_equal 5, @generator.get_sentences(5).count
+  end
 end
