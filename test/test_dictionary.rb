4108388348195807f7adb60c6a8992e5ddc0956f
justindomingue <justin.domingue@hotmail.com>
2014-12-31 18:37:55 -0500

    changed tests to suit new markov chains implementation

--- test/test_dictionary.rb
+++ test/test_dictionary.rb
@@ -5,16 +5,15 @@ class DictionaryTest < Minitest::Test
   def setup
     source = "Why did the chicken cross the road?"
     @order1 = MarkovChains::Dictionary.new(source)
-    @order2 = MarkovChains::Dictionary.new(source)
   end
 
   def test_get
     assert_equal "did", @order1.get(["Why"])
-    assert_equal true, %w(chicken road).include?( @order1.get(["the"]))
+    assert_equal true, %w(chicken road).include?(@order1.get(["the"]))
     assert_equal nil, @order1.get(["Not there..."])
   end
   
   def test_get_start_word
-    assert_equal ["Why"], @order1.get_start_word
+    assert_equal ["Why"], @order1.get_start_words
   end
 end
