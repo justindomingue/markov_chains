eac222a44aaa0e585f01b6d9c0a48bcf25689221
justindomingue <justin.domingue@hotmail.com>
2014-12-31 16:14:11 -0500

    rewrote markov chain algorithm to handle dictionary order

--- lib/markov_chains/dictionary.rb
+++ lib/markov_chains/dictionary.rb
@@ -3,73 +3,82 @@ module MarkovChains
     
     # Initialized the dictionary with a text source.
     #
-    # @example Create a new dictionary
+    # @example Create a new dictionary of order 1
     #   MarkovChains::Dictionary.new(string)
-    # @param [String] the text source
+    # @example Create a new dictinary of order 2
+    #   MarkovChains::Dictionary.new(string, 2)
     #
-    def initialize(text)
-      @words = Hash.new
+    # @param [String] the text source
+    # @param [int] the order of the dictionary. The order is the "memory" of the dictionary, meaning that an order <n> dictionary will consider <n> words to generate the next one. Order of 1 or 2 are typical. More than 3 and the generated sentences will be the same as the source.
+    #
+    def initialize(text, order = 1)
+      @order = order
+      @words_for = Hash.new
       @start_words = Array.new
       
-      wordlist = text.split
+      # Standarize input text
+      text.delete! "\n"
       
-      # Add first word as possible start word
-      @start_words.push wordlist[0]
+      # Process each sentence
       
-      # Process each word
-      wordlist.each_with_index do |word, index|
-          add(word, wordlist[index + 1]) if index <= wordlist.size - 2
+      # <sentences> has format sentence+terminator:
+      #   ["sent1", "term1", "sent2", "term2", ...]
+      seps = /([.!?]+)/
+      sentences = text.split seps
+      sentences.each_slice(2) { |s,t| process_sentence(s,t) }
+    end
+    
+    # Processes a single sentence with terminator
+    #
+    # @example Process a sentence
+    #   process_sentence("It is sunny today", "!")
+    #
+    # @param [String] sentence to process
+    # @param [Character] sentence terminator
+    #
+    private def process_sentence(sentence, terminator)
+      # Consider phrases/words/clauses separators when splitting
+      seps = "([,;:])"
+      
+      # Split <sentence> into words
+      words = sentence.gsub(/(#{seps})\s+/, '\1').split(/\s+|(#{seps})/)
+      words << terminator
+      
+      # Add <@order> start words to the list
+      @start_words << words[0, @order]
+      
+      # Add the words to the frequency hash <words_for>
+      until words.size < @order + 1 do
+        (@words_for[words[0, @order]] ||= []) << words[@order]
+        words.shift
       end
     end
   
-    # Returns a word based on the likelihood of it appearing after the input word
+    # Returns a word based on the likelihood of it appearing after the input array of words
     #
     # @example Get a word likely to appear next to the word 'It'
-    #   get('It')
+    #   get(['It'])           # => 'has'
+    # @example Get a word likely to appear next to the words 'It has been' (with a dictionary of order 2)
+    #   get(['It has'])  # => 'been'
     #
-    # @param [String] word for which we want a possible next word
-    # @return [String] word that is likely to follow the input word
+    # @param [[String]] array of words for which we want a possible next word
+    # @return [String] word that is likely to follow the input
     #
-    def get(word)
-      return "" if !@words[word]
-      followers = @words[word]
-      sum = followers.inject(0) {|sum,kv| sum += kv[1]}
-      random = rand(sum)+1
-      partial_sum = 0
-      next_word = followers.find do |word, count|
-        partial_sum += count
-        partial_sum >= random
-      end.first
-      next_word
+    def get(words)
+      (@words_for[words] || []).sample
     end
     
-    # Returns a word beginning a sentence seen in the source
+    # Returns a list of words beginning a sentence seen in the source
     #
-    # @example Get a start word
-    #   get_start_word()
-    # @return [String] a possible start word
+    # @example Get a start words
+    #   get_start_word    # => ['It', 'has']
+    #
+    # @return [[String]] array of words that could start a sentence
     #
     def get_start_word
       @start_words.sample
     end
-    
-  private
-    
-    # Adds word-next_word combination to the dictionary
-    #
-    # @example Adding a word in a order 1 dictionary
-    #   add_word("It", "has")
-    # @example Adding words in a order m dictionary
-    #   add_word("It has", "been")
-    #
-    # @param word [string] root word
-    # @param next_word [string] word following the root word
-    #
-    def add(word, next_word)
-      @words[word] = Hash.new(0) if !@words[word]
-      @words[word][next_word] += 1
-      @start_words.push(next_word) if word.end_with? '.' and !word.nil?
-    end
+
     
   end
 end
--- test/test_dictionary.rb
+++ test/test_dictionary.rb
@@ -4,16 +4,17 @@ require 'markov_chains'
 class DictionaryTest < Minitest::Test
   def setup
     source = "Why did the chicken cross the road?"
-    @dict = MarkovChains::Dictionary.new(source)
+    @order1 = MarkovChains::Dictionary.new(source)
+    @order2 = MarkovChains::Dictionary.new(source)
   end
-  
+
   def test_get
-    assert_equal "did", @dict.get("Why")
-    assert_equal true, %w(chicken road).include?( @dict.get("the"))
-    assert_equal "", @dict.get("Not there...")
+    assert_equal "did", @order1.get(["Why"])
+    assert_equal true, %w(chicken road).include?( @order1.get(["the"]))
+    assert_equal nil, @order1.get(["Not there..."])
   end
   
   def test_get_start_word
-    assert_equal "Why", @dict.get_start_word()
+    assert_equal ["Why"], @order1.get_start_word
   end
 end
